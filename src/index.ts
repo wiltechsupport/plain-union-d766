5f55ea39649d980a1aebd8f1f9505fb3a09e796d
wiltechsupport <william@wiltechservices.com>
2025-02-26 03:37:22 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "singing cyberpunk cats",
     };
 
     const response = await env.AI.run(
